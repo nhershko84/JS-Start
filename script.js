96d0b632f7e534f90c822c8ffc79b13a136dd971
nhershko84 <n.hershko84@gmail.com>
2024-01-08 17:13:52 -0600

    first commit

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // script.js
 
-// Dummy content for demonstration purposes
-const homeContent = "<p>Welcome to my portfolio homepage!</p>";
-const aboutContent = "<p>About me: I am a passionate web developer.</p>";
-const contactContent = "<p>Contact me at: N.Hershko84@Gmail.com</p>";
-const resumeContent = "<p>My resume will be available soon.</p>";
+// const homeContent = "<p>Welcome to my portfolio homepage!</p>";
+// const aboutContent = "<p>About me: I am a passionate web developer.</p>";
+// const contactContent = "<p>Contact me at: N.Hershko84@Gmail.com</p>";
+// const resumeContent = "<p>Download my resume: <a href='assets/Resume.pdf' download='YourResumeName.pdf'>Resume</a></p>";
 
-
+// Initial content
 document.getElementById('content').innerHTML = homeContent;
 
+// Event listeners for button clicks
 document.getElementById('homeBtn').addEventListener('click', () => {
     document.getElementById('content').innerHTML = homeContent;
 });
@@ -24,4 +24,68 @@ document.getElementById('contactBtn').addEventListener('click', () => {
 document.getElementById('resumeBtn').addEventListener('click', () => {
     document.getElementById('content').innerHTML = resumeContent;
 });
+
+document.getElementById('homeBtn').addEventListener('click', () => {
+  window.location.href = 'home.html'; // Adjust the path based on your project structure
+});
+
+// Repeat this for other buttons and HTML files
+
+
+
+
+
+// 
+
+
+
+
+
+
+// const homeContent = "<p>Welcome to my portfolio homepage!</p>";
+// const aboutContent = "<p>About me: I am a passionate web developer.</p>";
+// const contactContent = "<p>Contact me at: N.Hershko84@Gmail.com</p>";
+// const resumeContent = "<p>Download my resume: <a href= assets/Resume.pdf download='YourResumeName.pdf'>Resume</a></p>";
+
+// // script.js
+
+// // Event listeners for button clicks
+// document.getElementById('homeBtn').addEventListener('click', () => {
+//   window.location.href = 'home.html';
+// });
+
+// document.getElementById('aboutBtn').addEventListener('click', () => {
+//   window.location.href = 'about.html';
+// });
+
+// document.getElementById('contactBtn').addEventListener('click', () => {
+//   window.location.href = 'contact.html';
+// });
+
+// document.getElementById('resumeBtn').addEventListener('click', () => {
+//   window.location.href = 'resume.html';
+// });
+
+
+
+// Initial content
+// document.getElementById('content').innerHTML = homeContent;
+
+// Event listeners for button clicks
+// document.getElementById('homeBtn').addEventListener('click', () => {
+//     document.getElementById('content').innerHTML = homeContent;
+// });
+
+// document.getElementById('aboutBtn').addEventListener('click', () => {
+//     document.getElementById('content').innerHTML = aboutContent;
+// });
+
+// document.getElementById('contactBtn').addEventListener('click', () => {
+//     document.getElementById('content').innerHTML = contactContent;
+// });
+
+// document.getElementById('resumeBtn').addEventListener('click', () => {
+//     document.getElementById('content').innerHTML = resumeContent;
+// });
+
 
